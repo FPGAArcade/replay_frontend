5662c9f6e78b52c52f63a8ab39604085376658e0
Daniel Collin <daniel@collin.com>
2020-10-31 12:24:07 +0100

    Basic EGL setup

--- src/testbed_replay/src/main.rs
+++ src/testbed_replay/src/main.rs
@@ -1,46 +1,87 @@
 use khronos_egl as egl;
 
-fn init_egl() -> Result<(), egl::Error> {
-    let display = egl::get_display(egl::DEFAULT_DISPLAY)?;
-    let egl_ver = egl::initialize(display)?;
+#[derive(Debug)]
+struct EglContext {
+    display: egl::Display,
+    context: egl::Context,
+    surface: egl::Surface,
+}
 
-    println!("Found EGL ver {}.{}", egl_ver.0, egl_ver.1);
+impl EglContext {
+    pub fn new() -> Result<EglContext, egl::Error> {
+        let display = egl::get_display(egl::DEFAULT_DISPLAY)?;
+        let egl_ver = egl::initialize(display)?;
 
-    #[rustfmt::skip]
-    let config_attributes: &[egl::Int] = &[
-        egl::SAMPLES,             4,
-        egl::ALPHA_SIZE,          0,
-        egl::RED_SIZE,            8,
-        egl::GREEN_SIZE,          8,
-        egl::BLUE_SIZE,           8,
-        egl::BUFFER_SIZE,         32,
+        println!("Found EGL ver {}.{}", egl_ver.0, egl_ver.1);
 
-        egl::STENCIL_SIZE,        0,
-        egl::RENDERABLE_TYPE,     egl::OPENGL_ES2_BIT,
-        egl::SURFACE_TYPE,        egl::WINDOW_BIT,
-        egl::DEPTH_SIZE,          16,
-        egl::NONE
-    ];
+        #[rustfmt::skip]
+        let config_attributes: &[egl::Int] = &[
+            egl::SAMPLES,             4,
+            egl::ALPHA_SIZE,          0,
+            egl::RED_SIZE,            8,
+            egl::GREEN_SIZE,          8,
+            egl::BLUE_SIZE,           8,
+            egl::BUFFER_SIZE,         32,
 
-    #[rustfmt::skip]
-    let _context_attributes: &[egl::Int] = &[
-        egl::CONTEXT_CLIENT_VERSION, 2,
-        egl::NONE, egl::NONE,
-        egl::NONE
-    ];
+            egl::STENCIL_SIZE,        0,
+            egl::RENDERABLE_TYPE,     egl::OPENGL_ES2_BIT,
+            egl::SURFACE_TYPE,        egl::WINDOW_BIT,
+            egl::DEPTH_SIZE,          16,
+            egl::NONE
+        ];
 
-    // do 1 config for now as we have a fixed platform
-    let mut configs = Vec::with_capacity(1);
-    egl::choose_config(display, config_attributes, &mut configs)?;
+        #[rustfmt::skip]
+        let context_attributes: &[egl::Int] = &[
+            egl::CONTEXT_CLIENT_VERSION, 2,
+            egl::NONE, egl::NONE,
+            egl::NONE
+        ];
 
-    if configs.len() == 0 {
-        println!("Unable to find any EGL configs");
-        return Err(egl::Error::BadConfig);
+        // do 1 config for now as we have a fixed platform
+        let mut configs = Vec::with_capacity(1);
+        egl::choose_config(display, config_attributes, &mut configs)?;
+
+        if configs.len() == 0 {
+            println!("Unable to find any EGL configs");
+            return Err(egl::Error::BadConfig);
+        }
+
+        let surface =
+            unsafe { egl::create_window_surface(display, configs[0], std::ptr::null_mut(), None)? };
+
+        egl::bind_api(egl::OPENGL_ES_API)?;
+
+        let context = egl::create_context(display, configs[0], None, context_attributes)?;
+
+        Ok(EglContext {
+            display,
+            context,
+            surface,
+        })
     }
 
-    Ok(())
+    fn close(&self) -> Result<(), egl::Error> {
+        egl::make_current(self.display, None, None, Some(self.context))?;
+        egl::destroy_context(self.display, self.context)?;
+        egl::destroy_surface(self.display, self.surface)?;
+        egl::terminate(self.display)
+    }
+}
+
+impl Drop for EglContext {
+    fn drop(&mut self) {
+        match self.close() {
+            Ok(()) => (),
+            Err(e) => println!("Error when closing {}", e),
+        }
+    }
 }
 
 fn main() {
-    init_egl().unwrap();
+    let context = EglContext::new().unwrap();
+
+
+
+
+    dbg!(context);
 }
