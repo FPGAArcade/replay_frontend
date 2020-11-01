ed4b3b41b51e0f8fc177ff8a6afedd621342376a
Daniel Collin <daniel@collin.com>
2020-11-01 13:38:32 +0100

    Added libinput/udev libs
    Also added override of libudev-sys as it was trying to use pkg-config

--- src/testbed_replay/build.rs
+++ src/testbed_replay/build.rs
@@ -1,3 +1,6 @@
 fn main() {
+    println!("cargo:rustc-link-search=../../external/libinput");
+    println!("cargo:rustc-link-search=../../external/libudev");
     println!("cargo:rustc-link-search=../../external/mali");
+    println!("cargo:rustc-link-lib=udev");
 }
