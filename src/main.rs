e46afa9b74f0af5b58f852e373593bf13b602c29
Daniel Collin <daniel@collin.com>
2023-12-03 17:24:29 +0100

    Initial UI start

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,35 @@
-fn main() {
-    println!("Hello, world!");
+use flowi::window::{WindowFlags, Window};
+use flowi::button::Button;
+use flowi::Application;
+
+struct App {
+    dummy: u32,
 }
+
+fn main_loop(app: &mut App) {
+    Window::begin("Testing foobar", WindowFlags::NONE);
+
+    if Button::regular("Hello, world!") {
+        println!("Clicked!");
+    }
+
+    Window::end();
+}
+
+fn main() {
+    let settings = flowi::ApplicationSettings { 
+        width: 1280,
+        height: 720,
+    };
+
+    let mut flowi_app = Application::new(&settings).unwrap();
+
+    let app = Box::new(App {
+        dummy: 1337,
+    });
+
+    if !flowi_app.run(app, main_loop) {
+        println!("Failed to create main application");
+    }
+}
+
