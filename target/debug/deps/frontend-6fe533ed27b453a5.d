e26dbacdc12d0384f9f1e14b03a3a112aab3ce8f
Daniel Collin <daniel@collin.com>
2023-12-03 15:41:39 +0100

    Removed target

--- target/debug/deps/frontend-6fe533ed27b453a5.d
+++
@@ -1,5 +0,0 @@
-/home/emoon/code/projects/replay_frontend/target/debug/deps/frontend-6fe533ed27b453a5: src/main.rs
-
-/home/emoon/code/projects/replay_frontend/target/debug/deps/frontend-6fe533ed27b453a5.d: src/main.rs
-
-src/main.rs:
